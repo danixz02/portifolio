45d1641686cca44c84210d0c117b18039b9cbc98
Hanakoo <danielmobile098@gmail.com>
2025-02-05 21:27:02 -0300

    exclusão de som no menu

--- script.js
+++ script.js
@@ -1,37 +1,9 @@
-async function requestAudioPermission() {
-  if (navigator.permissions) {
-    try {
-      const permission = await navigator.permissions.query({ name: "microphone" });
-      console.log("Permissão de áudio:", permission.state);
-    } catch (error) {
-      console.warn("O navegador não suporta permissão explícita para áudio.");
-    }
-  }
-}
-
-async function toggleMenu() {
+function toggleMenu() {
   const menu = document.querySelector('.menu-ul-menu-hamburguer');
   menu.classList.toggle('active');
-
-  const sound = document.getElementById("menuSound");
-
-  try {
-    await requestAudioPermission(); // Solicita permissão antes de tocar o som
-
-    if (sound.paused) {
-      sound.currentTime = 0;
-      await sound.play();
-    } else {
-      sound.pause();
-      sound.currentTime = 0;
-    }
-  } catch (error) {
-    console.error("Erro ao tocar o som:", error);
-  }
 }
 
-// Solicita permissão ao carregar a página (opcional)
-document.addEventListener("DOMContentLoaded", requestAudioPermission);
+
   document.getElementById("download-btn").addEventListener("click", function() {
     // Criar um link temporário
     let link = document.createElement("a");
