becfd0aaab4cfa371c5a9fbc8ee4963700a34ac9
Hanakoo <danielmobile098@gmail.com>
2025-02-06 06:36:28 -0300

    Melhora na organização

---
+++ src/scripts/script.js
@@ -0,0 +1,25 @@
+function toggleMenu() {
+  const menu = document.querySelector('.menu-ul-menu-hamburguer');
+  menu.classList.toggle('active');
+}
+
+
+  document.getElementById("download-btn").addEventListener("click", function() {
+    // Criar um link temporário
+    let link = document.createElement("a");
+    link.href = "./files/Daniel Friedrich de Moura.pdf";
+    link.download = "Daniel Friedrich - CV.pdf"; // Nome do arquivo ao baixar
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+});
+
+
+document.getElementById("contact-btn").addEventListener("click", function() {
+  let phone = "5527995892136"; 
+  let message = "Olá! Acessei seu portfólio e estou interessado(a) em seu trabalho. Podemos conversar?";
+  let url = `https://wa.me/${phone}?text=${encodeURIComponent(message)}`;
+
+  window.open(url, "_blank"); // Abre em uma nova aba
+});
+
